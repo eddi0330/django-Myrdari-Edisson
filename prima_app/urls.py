ba6f15be2ef79422f0ca1202fe28bddf52855a91
eddi0330 <92153436+eddi0330@users.noreply.github.com>
2021-11-23 09:43:38 +0100

    push

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from prima_app.views import homepage,welcome,lista,chi_siamo,variabili,index
 
+
 app_name="prima_app"
 urlpatterns=[
     path('homepage',homepage, name='homepage'),
@@ -8,5 +9,5 @@ urlpatterns=[
     path('lista',lista, name='lista'),
     path('chi_siamo',chi_siamo, name='chi_siamo'),
     path('variabili',variabili, name='variabili'),
-    path('index',index, name='index'),
+    path('',index, name='index'),
 ]
