81b526a670f6fa39094feca8b9c6d8e2798f7582
eddi0330 <92153436+eddi0330@users.noreply.github.com>
2021-12-09 12:13:31 +0100

    commit("Livello 12")

--- news/models.py
+++ news/models.py
@@ -6,14 +6,19 @@ class Giornalista(models.Model):
     nome=models.CharField(max_length=20)
     cognome=models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.nome+" "+ self.cognome
+    #def __str__(self):
+        #return self.nome+" "+ self.cognome
+    class Meta:
+        verbose_name="Giornalista"
+        verbose_name_plural="Giornalisti"
 
 class Articolo(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
-    
+    class Meta:
+        verbose_name="Articolo"
+        verbose_name_plural="Articoli"
 
-    def __str__(self):
-        return self.titolo
+    #def __str__(self):
+        #return self.titolo
